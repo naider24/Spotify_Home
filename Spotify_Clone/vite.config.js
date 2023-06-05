ebc168658c7d4d94290209afbd51b83464c07b9d
nicollas <nicollaswilker@gmail.com>
2023-06-05 18:21:57 -0300

    Spotify Home

--- Spotify_Clone/vite.config.js
+++ Spotify_Clone/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    open: '/index.html',
+    port:5173
+  },
+  
 })
